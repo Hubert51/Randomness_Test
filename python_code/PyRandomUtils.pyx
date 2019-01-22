35e81034fb8444a2bb3867f68c16c75ff609d9a0
Thomas McManus <mcmanustfj@gmail.com>
2019-01-22 13:51:15 -0500

    Created shuffle method for PyRandomUtils

--- python_code/PyRandomUtils.pyx
+++ python_code/PyRandomUtils.pyx
@@ -1,6 +1,8 @@
 
 from libc.stdint cimport uint32_t, uint64_t, UINT32_MAX
-
+import numpy as np
+cimport numpy as np
+import random
 
 
 
@@ -10,7 +12,25 @@ cdef class PRNG(object):
         return 4
 
     cpdef float rand(self) except *:
-        return 4 #guaranteed to be random by fair dice roll
+        return 0.4 #guaranteed to be random by fair dice roll
+
+    def randint(self, lo, hi):
+        """random int in [lo, hi)"""
+        r = self.rand() #idk the best way
+        while r==1:
+            r = self.rand()
+        return int(self.rand() * (hi-lo) + lo)
+
+cdef class PyRandGen(PRNG):
+
+    def __init__(self, seed=None):
+        random.seed(seed)
+
+    cpdef float rand(self) except *:
+        return random.random()
+
+    cpdef randint(self, lo, hi):
+        return random.randrange(start=lo, stop=hi)
 
 
 cdef class LCG(PRNG):
@@ -26,10 +46,12 @@ cdef class LCG(PRNG):
         self.a = a
 
     cpdef uint32_t randi(self):
+        '''Returns an int between 0 and self.mod'''
         self.state = (self.a * self.state) % self.mod
         return self.state
 
     cpdef float rand(self):
+        '''returns a float between 0 and 1'''
         return (<float> self.randi()) / self.mod
 
 cpdef LCG LCG_RANDU(seed):
@@ -57,3 +79,24 @@ cdef class MiddleSquare_WeylSequence(PRNG):
 
     cpdef float rand(self):
         return (<float>self.randi())/UINT32_MAX
+
+
+# We now need to fix a datatype for our arrays. I've used the variable
+# DTYPE for this, which is assigned to the usual NumPy runtime
+# type info object.
+CARD = np.int8
+
+# "ctypedef" assigns a corresponding compile-time type to DTYPE_t. For
+# every type in the numpy module there's a corresponding compile-time
+# type with a _t-suffix.
+ctypedef np.int8_t CARD_T
+
+ctypedef np.npy_intp IDX_t
+
+
+def shuffle(PRNG gen, np.ndarray[CARD_T, ndim=1] arr):
+    cdef IDX_t i, j, n=len(arr)
+    for i in range(0, n-2):
+        j = gen.randint(i, n)
+        arr[i], arr[j] = arr[j], arr[i]
+
