05e92a0844e662af7ed1b4bc651a28dafe3f2455
Hubert51 <ruijiegeng@gmail.com>
2019-03-27 11:21:34 -0400

    small changes in utils

--- function_test/generate_ts.py
+++ function_test/generate_ts.py
@@ -1,19 +1,26 @@
+import sys, os
+
+sys.path.append(os.path.dirname(os.getcwd()))
 import utils.toolbox as toolbox
 import unittest
 import numpy as np
 
-
-
 if __name__ == '__main__':
-    models = [ ["MT"], ["hardware"] ]
-    # models = []
-    for i in range(10, 30, 5):
-        models.append(["LCG", i])
-
+    # models = [ ["Halton"], ]
+    # models = [ ["MT"] ]
+    models = [["bigdeal", "10000000"]]
+    models = [[toolbox.REAL_GAME, "1_year", ["AFT"]]]
+    models = [[toolbox.HALTON]]
+    # for i in range(10, 30, 5):
+    #     models.append(["LCG", i])
     batch_size = 1
-    batch_num = int(10 ** 7)
+    batch_num = int(10000000)
 
     for model in models:
         result = toolbox.generate_ts(batch_size, batch_num, model)
-        file_name = ''.join(str(x) for x in model) + ".txt"
+        if len(model) >= 2:
+            file_name = toolbox.MODEL_NAME[model[0]] + model[1] + ".txt"
+        else:
+            file_name = toolbox.MODEL_NAME[model[0]] + ".txt"
+
         np.savetxt(file_name, result)
--- utils/CardUtils.py
+++ utils/CardUtils.py
@@ -1,5 +1,6 @@
 # from libc.stdint cimport uint32_t, uint64_t, UINT32_MAX
 import numpy as np
+
 # cimport numpy as np
 
 # ctypedef np.int8_t CARD_T
@@ -44,17 +45,16 @@ theoretical_probabilities = np.array([
     0.3055,
     0.5380,
     0.1054,
-    0.00705+0.00651,
+    0.00705 + 0.00651,
     0.00072,
     0.000056,
     0.285846,
-    0.801244- 0.285846,
+    0.801244 - 0.285846,
     0.985549 - 0.801244,
     1 - 0.985549
-    ])
+])
 
-
-fdict = {item : index for index, item in enumerate(feature_string) }
+fdict = {item: index for index, item in enumerate(feature_string)}
 
 num_features = len(feature_string)
 
@@ -64,16 +64,20 @@ def get_num_features():
 
 
 def val_card(card):
-    return (card+1)%13
+    return (card + 1) % 13
+
 
 def val_card_name(card):
-    return names[card%13]
+    return names[card % 13]
+
 
 def suit_card(card):
     return suits[(card - 1) // 13]
 
+
 def name_card(card):
     return '{}{}'.format(val_card_name(card), suit_card(card))
+
 
 def get_dist(hand):
     dist = [0, 0, 0, 0]
@@ -81,22 +85,24 @@ def get_dist(hand):
         dist[(c - 1) // 13] += 1
     return sorted(dist, reverse=True)
 
+
 def print_all_dist(deal):
-    for hand in np.reshape(deal, (4,13)):
+    for hand in np.reshape(deal, (4, 13)):
         print(get_dist(hand))
 
+
 def get_points(hand):
-    return sum((max(0, (card-1) % 13 - 8) for card in hand))
+    return sum((max(0, (card - 1) % 13 - 8) for card in hand))
 
 
-def get_features(deal): 
+def get_features(deal):
     features = np.zeros(num_features, dtype=np.float)
-    for hand in np.reshape(deal, (4,13)):
+    for hand in np.reshape(deal, (4, 13)):
         dist = get_dist(hand)
         longest = dist[0]
 
-        if ( longest < 12 ) :
-            features[fdict['ls'+str(longest)]]+=1
+        if (longest < 12):
+            features[fdict['ls' + str(longest)]] += 1
 
         if longest == 6 and dist[1] == 5:
             features[fdict['6-5']] += 1
@@ -108,7 +114,7 @@ def get_features(deal):
             features[fdict['7-6']] += 1
 
         shortest = dist[-1]
-        features[fdict['ss'+str(shortest)]]+=1
+        features[fdict['ss' + str(shortest)]] += 1
 
         points = get_points(hand)
         if points < 8:
@@ -121,6 +127,5 @@ def get_features(deal):
             features[fdict['p19+']] += 1
 
     return features / 4
-
 
 
